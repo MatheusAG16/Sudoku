936157cc2ffda29e1015dfdf15c3748ff74c0e8c
MatheusAG16 <mateusandradegermano@gmail.com>
2024-12-06 17:42:11 -0300

    Atualização de Efeitos
    Pequena correção nos efeitos da seleção de Célula.
    
    - Agora quando criamos um novo jogo, toda a seleção é restaurada.

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ document.addEventListener('DOMContentLoaded', function gerarTabuleiro(){
                 c.classList.remove('cell-shining')
             })
         })
+<<<<<<< Updated upstream
 
         //Função para selecionar a célula com um clique, fazendo com que todas as céulas iguais sejam alteradas
         cell.addEventListener('click', function selectCell(event) {
@@ -121,6 +122,32 @@ document.addEventListener('DOMContentLoaded', function gerarTabuleiro(){
                 }
             })
             console.log(selectedCell)
+=======
+        cell.addEventListener('click', function preencherColRow(){
+
+            let coluna = cell.getAttribute('data-column')
+            let linha = cell.getAttribute('data-row')
+
+
+            let selected = [coluna, linha]
+
+
+            if(!selected){
+                document.querySelectorAll('.cell-selected').forEach((c) => {
+                    c.classList.remove('cell-selected')
+                    selected = true
+                })
+
+                document.querySelectorAll(`.cell[data-column="${column}"], .cell[data-row="${row}"]`).forEach((c) => {
+                    c.classList.add('cell-selected')
+                })
+            } if(selected) {
+                document.querySelectorAll('.cell-selected').forEach((c) => {
+                    c.classList.remove('cell-selected')
+                })
+            }
+
+>>>>>>> Stashed changes
         })
     })
 })
